3f9f007b6a30c81988ab5252a8bd831fef0823b1
n00231001 <n00231001@iadt.ie>
2025-03-23 22:33:38 +0000

    bootstrap

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,14 +13,14 @@
     );
     ?>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+    <div class="py-12 bg-gray">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-10 rounded-lg" style="background-color: #e1e2e3; rounded-lg">
+            <div class="bg-gray overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 bg-gray border: 4px solid black; padding: 10px; rounded-lg ">
                     {{ __("You're logged in!") }}
                     
                     <!-- Container for the gauges -->
-                    <div style="display: flex; justify-content: space-between;">
+                    <div style="display: flex; justify-content: space-between; border: 4px solid black; padding: 10px; rounded-lg">
                         <!-- First Gauge -->
                         <div id="myGaugeChart1" style="width:600px;height:400px;position:relative;"></div>
                         <!-- Second Gauge -->
@@ -28,11 +28,12 @@
                     </div>
                     <br>
                     <!-- Bootstrap row for cards -->
-                    <div class="row">
+                    <div class="row bg-gray border-primary rounded-lg">
+                        <br>
                         <!-- first card -->
-                        <div class="col-md-6 ">
-                            <div class="card text-white bg-secondary mb-3 ">
-                                <div class="card-header text-center ">Current Electricity Spending</div>
+                        <div class="col-md-6">
+                            <div class="card text-white bg-dark mb-3 border border-dark">
+                                <div class="card-header text-center">Current Electricity Spending</div>
                                 <div class="card-body">
                                     <h5 class="card-title text-center">Electricity Usage</h5>
                                     <h1 class="card-title text-center">$300</h1>
@@ -42,20 +43,23 @@
                         </div>
                         <!-- second card -->
                         <div class="col-md-6">
-                            <div class="card text-white bg-secondary mb-3 justify center">
+                            <div class="card text-white bg-dark mb-3 border border-dark">
                                 <div class="card-header text-center">Current Heating Spending</div>
                                 <div class="card-body text-center">
                                     <h5 class="card-title">Heating Usage</h5>
                                     <h1 class="card-title text-center">$500</h1>
-                                    <img src="{{ asset('images/fire-solid.svg') }}" width="80" alt="Electricity Usage Icon" class="mx-auto d-block">
+                                    <img src="{{ asset('images/fire-solid.svg') }}" width="80" alt="Heating Usage Icon" class="mx-auto d-block">
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <br>
+                    
                     <!-- Add a div for the CanvasJS column chart -->
-                     <h2>previous spending</h2>
-                    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+                    <div class="p-6 text-gray-900 bg-gray border: 4px solid black; padding: 10px; rounded-lg ">
+                        <h2>Previous Spending</h2>
+                    <div id="chartContainer" style="height: 370px; width: 100%; "></div>
                     <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,8 +86,10 @@
         ];
 
         var layout1 = { 
-            width: 600, 
+            width: 550, 
             height: 400,
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
             images: [
                 {
                     source: "{{ asset('images/bolt-solid.svg') }}",
@@ -109,14 +115,15 @@
                 type: "indicator",
                 mode: "gauge+number",
                 delta: { reference: 400 },
-                gauge: { axis: { range: [null, 500] },
-                bar: { color: "FF7556"} }
+                gauge: { axis: { range: [null, 500] }, bar: { color: "FF7556", thickness: 0.5 } }
             }
         ];
 
         var layout2 = { 
-            width: 600, 
+            width: 550, 
             height: 400,
+            paper_bgcolor: 'rgba(0,0,0,0)',
+            plot_bgcolor: 'rgba(0,0,0,0)',
             images: [
                 {
                     source: "{{ asset('images/fire-solid.svg') }}",
@@ -134,24 +141,85 @@
 
     <!-- Initialize CanvasJS Column Chart -->
     <script>
-    window.onload = function() {
-        var chart = new CanvasJS.Chart("chartContainer", {
-            animationEnabled: true,
-            theme: "light2",
-            title:{
-                text: "Gold Reserves"
-            },
-            axisY: {
-                title: "Gold Reserves (in tonnes)"
-            },
-            data: [{
-                type: "column",
-                yValueFormatString: "#,##0.## tonnes",
-                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-            }]
-        });
-        chart.render();
-    }
+        window.onload = function () {
+            var chart = new CanvasJS.Chart("chartContainer", {
+                animationEnabled: true,
+                title:{
+                    text: "Daily High Temperature at Different Beaches"
+                },
+                axisX: {
+                    valueFormatString: "DD MMM,YY"
+                },
+                axisY: {
+                    title: "Temperature (in °C)",
+                    suffix: " °C"
+                },
+                legend:{
+                    cursor: "pointer",
+                    fontSize: 16,
+                    itemclick: toggleDataSeries
+                },
+                toolTip:{
+                    shared: true
+                },
+                data: [{
+                    name: "Myrtle Beach",
+                    type: "spline",
+                    yValueFormatString: "#0.## °C",
+                    showInLegend: true,
+                    dataPoints: [
+                        { x: new Date(2017,6,24), y: 31 },
+                        { x: new Date(2017,6,25), y: 31 },
+                        { x: new Date(2017,6,26), y: 29 },
+                        { x: new Date(2017,6,27), y: 29 },
+                        { x: new Date(2017,6,28), y: 31 },
+                        { x: new Date(2017,6,29), y: 30 },
+                        { x: new Date(2017,6,30), y: 29 }
+                    ]
+                },
+                {
+                    name: "Martha Vineyard",
+                    type: "spline",
+                    yValueFormatString: "#0.## °C",
+                    showInLegend: true,
+                    dataPoints: [
+                        { x: new Date(2017,6,24), y: 20 },
+                        { x: new Date(2017,6,25), y: 20 },
+                        { x: new Date(2017,6,26), y: 25 },
+                        { x: new Date(2017,6,27), y: 25 },
+                        { x: new Date(2017,6,28), y: 25 },
+                        { x: new Date(2017,6,29), y: 25 },
+                        { x: new Date(2017,6,30), y: 25 }
+                    ]
+                },
+                {
+                    name: "Nantucket",
+                    type: "spline",
+                    yValueFormatString: "#0.## °C",
+                    showInLegend: true,
+                    dataPoints: [
+                        { x: new Date(2017,6,24), y: 22 },
+                        { x: new Date(2017,6,25), y: 19 },
+                        { x: new Date(2017,6,26), y: 23 },
+                        { x: new Date(2017,6,27), y: 24 },
+                        { x: new Date(2017,6,28), y: 24 },
+                        { x: new Date(2017,6,29), y: 23 },
+                        { x: new Date(2017,6,30), y: 23 }
+                    ]
+                }]
+            });
+            chart.render();
+
+            function toggleDataSeries(e){
+                if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                }
+                else{
+                    e.dataSeries.visible = true;
+                }
+                chart.render();
+            }
+        }
     </script>
 
     <!-- Include Bootstrap JS and Popper.js -->
